b6e61f5fa12f5bac8971de4f60de718406736d32
hermasRR <hermasr58@gmail.com>
2023-03-06 00:47:37 -0600

    mensaje de commit

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+datos = []
+
+db = mysql.connector.connect(
+    host="172.21.0.2",
+    user="usrTesting",
+    password="dbtesting",
+    database="DBtesting"
+)
 
 @app.route('/')
 def inicio():
     return render_template("index.html")
 
-@app.route('/enviarForm', methods=['POST'])
-def enviarForm():
+@app.route('/procesarForm', methods=['GET','POST'])
+def procesarForm():
+    codigo = request.form.get("codigo")
     nombre = request.form.get("nombre")
-    apellido = request.form.get("apellido")
-    edad = request.form.get("edad")
-    fecha_nacimiento = request.form.get("fecha_nacimiento")
-    
-    #return render_template("received.html", nombre=nombre, apellido=apellido, fecha_nacimiento=fecha_nacimiento, edad=edad)
-    return redirect(f'/datosRecibidos?nombre={nombre}&apellido={apellido}&fecha_nacimiento={fecha_nacimiento}&edad={edad}')
+    carrera = request.form.get("carrera")
+    datos.append({"codigo": codigo, "nombre": nombre, "carrera": carrera})
 
-@app.route('/datosRecibidos')
-def datosRecibidos():
-    nombre = request.args.get("nombre")
-    apellido = request.args.get("apellido")
-    edad = request.args.get("edad")
-    fecha_nacimiento = request.args.get("fecha_nacimiento")
-    return render_template("received.html", nombre=nombre, apellido=apellido, fecha_nacimiento=fecha_nacimiento, edad=edad)
+    cursor = db.cursor()
+
+    query = "INSERT INTO cursos (codigo, nombre, carrera) VALUES (%s, %s, %s)"
+    values = (codigo, nombre, carrera)
+    cursor.execute(query, values)
+    
+    db.commit()
+
+    #db.close()
+
+    #return "¡Gracias por enviar el formulario!"
+    return render_template("index.html", codigo=codigo, nombre=nombre, carrera=carrera, datos=datos)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- test_Selapp.py
+++ test_Selapp.py
@@ -10,7 +10,7 @@ def driver():
     #options.add_argument('--headless') # Ejecutar el navegador en modo headless (sin interfaz gráfica)
     #driver = webdriver.Chrome(options=options)
     
-    service = Service(executable_path="/usr/local/bin/chromedriver")
+    service = Service(executable_path="/chromedriver")
     driver = webdriver.Chrome(service=service)
     yield driver # Asegurarse de que el driver está disponible globalmente
     driver.quit() # Cerrar el driver después de que se han ejecutado todas las pruebas
@@ -22,17 +22,14 @@ def test_envio_formulario(driver):
     print(title)
 
     # Encuentra el elemento de entrada de texto en la página y escribe algo en él
+    codigo = driver.find_element(By.NAME, "codigo")
     nombre = driver.find_element(By.NAME, "nombre")
-    apellido = driver.find_element(By.NAME, "apellido")
-    fecha_nacimiento = driver.find_element(By.NAME, "fecha_nacimiento")
-    edad = driver.find_element(By.NAME, "edad")
+    carrera = driver.find_element(By.NAME, "carrera")
 
-    nombre.send_keys('Hermas')
-    apellido.send_keys('Ramirez')
-    fecha_nacimiento .send_keys('01-03-2000')
-    edad.send_keys('22')
-
-    #nombre.send_keys(Keys.RETURN)
+    codigo.send_keys('785')
+    nombre.send_keys('Analisis y Diseño de Sistemas 2')
+    carrera.send_keys('Ingenieria en Ciencias y Sistemas')
+ 
     submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
     submit_button.click()
 
@@ -44,26 +41,24 @@ def test_envio_formulario(driver):
     
     assert value == "Datos Guardados" 
 
-    assert 'Hermas' in driver.page_source
-    assert 'Ramirez' in driver.page_source
-    assert '2000-03-01' in driver.page_source
-    assert '22' in driver.page_source
+    assert '785' in driver.page_source
+    assert 'Analisis y Diseño de Sistemas 2' in driver.page_source
+    assert 'Ingenieria en Ciencias y Sistemas' in driver.page_source
 
 def test_elemento_existe(driver):
     # Cargar la página web
     driver.get('http://localhost:5000/')
 
+    codigo = driver.find_element(By.NAME, "codigo")
     nombre = driver.find_element(By.NAME, "nombre")
-    apellido = driver.find_element(By.NAME, "apellido")
-    fecha_nacimiento = driver.find_element(By.NAME, "fecha_nacimiento")
-    edad = driver.find_element(By.NAME, "edad")
+    carrera = driver.find_element(By.NAME, "carrera")
     submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
-    assert nombre.is_displayed()   
-    assert apellido.is_displayed() 
-    assert fecha_nacimiento .is_displayed() 
-    assert edad .is_displayed() 
+    assert codigo.is_displayed() 
+    assert nombre.is_displayed()    
+    assert carrera.is_displayed() 
     assert submit_button.is_displayed() 
+
 
 def test_elemento_no_existe(driver):
     # Cargar la página web
@@ -72,7 +67,11 @@ def test_elemento_no_existe(driver):
     # Intentar encontrar un elemento que no existe en la página
     non_existent_element = driver.find_element_by_css_selector('input[type="checkbox"]', 'input[type="radio"]')    
     # Esto debería generar un error, ya que el elemento no existe
-    assert non_existent_element.is_displayed()    
+    assert non_existent_element.is_displayed()  
+   
+    
+      
+
 
 
 
